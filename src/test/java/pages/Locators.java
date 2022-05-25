81f9541b0b6c1a2d53b5a589b76d72792ec9f8e0
Aurelija <aurelija.zemkute@gmail.com>
2022-05-25 16:07:24 +0300

    Create locations for InputFormSubmit class

--- src/test/java/pages/Locators.java
+++ src/test/java/pages/Locators.java
@@ -34,26 +34,51 @@ public class Locators {
             public static By inputByValue(String value) {
                 return By.xpath("//*[@name='optradio' and @value='" + value + "']");
             }
+
             public static By buttonGetCheckedValue = By.xpath("//*[@id='buttoncheck']");
             public static By fieldCheckedValue = By.xpath("//*[@class='radiobutton']");
-//           male ir female
+
+            //           male ir female
             public static By inputBySex(String sex) {
                 return By.xpath("//*[@name='gender' and @value='" + sex + "']");
             }
-//            age group
+
+            //            age group
             public static By inputByAgeGroup(String ageGroup) {
                 return By.xpath("//*[@name='ageGroup' and @value='" + ageGroup + "']");
             }
+
             public static By buttonGetValues = By.xpath("(//*[@type='button'])[3]");
             public static By fieldSexAndAgeGroup = By.xpath("//*[@class='groupradiobutton']");
         }
 
-        public static class JavascriptAlerts{
+        public static class JavascriptAlerts {
             public static By buttonForAlertBox = By.xpath("//*[@class='btn btn-default']");
             public static By buttonForConfirmBox = By.xpath("//*[@class='btn btn-default btn-lg' and text()='Click me!']");
             public static By buttonForPromptBox = By.xpath("//*[@class='btn btn-default btn-lg' and text()='Click for Prompt Box']");
             public static By fieldConfirmBoxMessage = By.xpath("//*[@id='confirm-demo']");
             public static By fieldPromptBoxMessage = By.xpath("//*[@id='prompt-demo']");
+        }
+
+        public static class InputFormSubmit {
+            public static By inputFirstName = By.xpath("//*[@name='first_name']");
+            public static By inputLastName = By.xpath("//*[@name='last_name']");
+            public static By inputEmail = By.xpath("//*[@name='email']");
+            public static By inputPhone = By.xpath("//*[@name='phone']");
+            public static By inputAddress = By.xpath("//*[@name='address']");
+            public static By inputCity = By.xpath("//*[@name='city']");
+            public static By selectState = By.xpath("//*[@name='state']");
+            public static By inputZipCode = By.xpath("//*[@name='zip']");
+            public static By inputWebsiteName = By.xpath("//*[@name='website']");
+
+            //            radiobuttons yes or no
+            public static By inputYesOrNoByValue(String value) {
+                return By.xpath("//*[@type='radio' and @value='" + value + "']");
+            }
+
+            public static By inputProjectDescription = By.xpath("//*[@name='comment']");
+            public static By buttonSend = By.xpath("//*[@class='btn btn-default']");
+
         }
     }
 }
